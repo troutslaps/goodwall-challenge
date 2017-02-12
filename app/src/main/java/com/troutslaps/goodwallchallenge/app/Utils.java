d2c8ef37a8ee73bb1889bb3a4f9ef108c1c5ceb4
Mai Cervantes <kikiam@gmail.com>
2017-02-13 00:09:45 +0800

    Utils.java - added util methods for new date formats and for displaying locality

--- app/src/main/java/com/troutslaps/goodwallchallenge/app/Utils.java
+++ app/src/main/java/com/troutslaps/goodwallchallenge/app/Utils.java
@@ -1,5 +1,12 @@
 package com.troutslaps.goodwallchallenge.app;
 
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
+
+import com.troutslaps.goodwallchallenge.R;
+import com.troutslaps.goodwallchallenge.model.Location;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,6 +25,24 @@ public class Utils {
 
             };
 
+    private static ThreadLocal<SimpleDateFormat> sdfForView = new
+            ThreadLocal<SimpleDateFormat>() {
+                @Override
+                protected SimpleDateFormat initialValue() {
+                    return new SimpleDateFormat(Constants.View.DateFormat);
+                }
+
+            };
+
+    private static ThreadLocal<SimpleDateFormat> sdfForTime = new
+            ThreadLocal<SimpleDateFormat>() {
+                @Override
+                protected SimpleDateFormat initialValue() {
+                    return new SimpleDateFormat(Constants.View.TimeFormat);
+                }
+
+            };
+
     public static class DateTime {
         public static Date parseFromApi(String string) {
             try {
@@ -27,7 +52,44 @@ public class Utils {
             }
             return new Date();
         }
+
+        public static String getFormattedDate(Date date) {
+            if (date == null) {
+                return "";
+            }
+            return sdfForView.get().format(date);
+        }
+
+        public static String getTimeAgo(Context context, Date date) {
+            return DateUtils.getRelativeTimeSpanString(date.getTime(), new Date().getTime(),
+                    DateUtils.MINUTE_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString();
+        }
+
+        public static String getDisplayTime(Date date) {
+            if (date == null) {
+                return "";
+            }
+            return sdfForTime.get().format(date);
+        }
     }
 
 
+    public static class Locality {
+        public static String getLocalityDisplay(Context context, Location location) {
+            String locality = "";
+            if (location != null) {
+                if (location.getCountry() != null && location.getLocality() != null) {
+                    locality = String.format(context.getString(R.string.complete_locality),
+                            location.getLocality(), location.getCountry());
+                } else if (location.getCountry() != null) {
+                    locality = String.format(context.getString(R.string.locality), location
+                            .getCountry());
+                } else {
+                    locality = String.format(context.getString(R.string.locality), location
+                            .getLocality());
+                }
+            }
+            return locality;
+        }
+    }
 }
