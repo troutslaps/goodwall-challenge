84034dfcb7cea82ca4bdd03447ad54c5d5d6406e
Mai Cervantes <kikiam@gmail.com>
2017-02-16 11:09:50 +0800

    CommentsFragment.java - played around with change listeners

--- app/src/main/java/com/troutslaps/goodwallchallenge/view/fragment/CommentsFragment.java
+++ app/src/main/java/com/troutslaps/goodwallchallenge/view/fragment/CommentsFragment.java
@@ -14,11 +14,13 @@ import com.troutslaps.goodwallchallenge.BR;
 import com.troutslaps.goodwallchallenge.R;
 import com.troutslaps.goodwallchallenge.databinding.FragmentCommentsBinding;
 import com.troutslaps.goodwallchallenge.model.Achievement;
+import com.troutslaps.goodwallchallenge.model.Comment;
 import com.troutslaps.goodwallchallenge.view.adapter.CommentAdapter;
 import com.troutslaps.goodwallchallenge.viewmodel.AchievementViewModel;
 
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,14 +28,15 @@ import io.realm.RealmChangeListener;
 public class CommentsFragment extends Fragment {
 
     private static final String TAG = CommentsFragment.class.getSimpleName();
-    Achievement achievement;
     Realm realm;
     RealmChangeListener<Achievement> achievementChangeListener;
+    RealmChangeListener<RealmResults<Comment>> commentsChangeListener;
     CommentsFragmentListener listener;
     FragmentCommentsBinding binding;
     RecyclerView commentsRv;
     CommentAdapter commentsAdapter;
 
+    Achievement achievement;
 
     public CommentsFragment() {
     }
@@ -42,14 +45,14 @@ public class CommentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_comments, container, false);
+        setupRecyclerView(binding.getRoot());
         return binding.getRoot();
     }
 
     private void setupRecyclerView(View v) {
         commentsRv = (RecyclerView) v.findViewById(R.id.list);
         commentsRv.setLayoutManager(new LinearLayoutManager(getActivity()));
-        commentsAdapter = new CommentAdapter(getActivity(), achievement.getComments());
-        commentsRv.setAdapter(commentsAdapter);
+
     }
 
     @Override
@@ -61,17 +64,27 @@ public class CommentsFragment extends Fragment {
             listener = (CommentsFragmentListener) context;
             realm = Realm.getDefaultInstance();
             achievement = Achievement.getAchievement(realm, listener.getAchievementId());
+
+
             achievementChangeListener = new RealmChangeListener<Achievement>() {
                 @Override
                 public void onChange(Achievement element) {
+                    if (commentsAdapter == null) {
+                        commentsAdapter = new CommentAdapter(getActivity(), achievement
+                                .getComments());
+                        commentsRv.setAdapter(commentsAdapter);
+                    } else {
+                        commentsAdapter.notifyDataSetChanged();
+                    }
                     binding.setVariable(BR.viewModel, new AchievementViewModel
                             (getContext(),
-                            achievement, (AchievementViewModel.Listener) getActivity()));
-                    setupRecyclerView(binding.getRoot());
-                    commentsAdapter.notifyDataSetChanged();
+                                    achievement, (AchievementViewModel.Listener) getActivity()));
+
+
                 }
             };
             achievement.addChangeListener(achievementChangeListener);
+
         } else {
             throw new IllegalArgumentException();
         }
