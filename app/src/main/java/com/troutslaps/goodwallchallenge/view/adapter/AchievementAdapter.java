e485e47b5b831ae9b3b08dd8000377a2e03d24f5
Mai Cervantes <kikiam@gmail.com>
2017-02-18 16:32:06 +0800

    AchievementAdapter.java - removed extra call to notifyChange()

--- app/src/main/java/com/troutslaps/goodwallchallenge/view/adapter/AchievementAdapter.java
+++ app/src/main/java/com/troutslaps/goodwallchallenge/view/adapter/AchievementAdapter.java
@@ -44,7 +44,6 @@ public class AchievementAdapter extends RecyclerView.Adapter<AchievementAdapter.
         ItemAchievementBinding postBinding = holder.binding;
         AchievementViewModel vm = postBinding.getAchievementViewModel();
         vm.setAchievement(achievements.get(position));
-        postBinding.notifyChange();
     }
 
     @Override
