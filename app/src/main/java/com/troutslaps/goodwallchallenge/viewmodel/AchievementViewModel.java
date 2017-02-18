6ef70a9592d76dc7835cb34b5e3372426359ef07
Mai Cervantes <kikiam@gmail.com>
2017-02-18 16:32:36 +0800

    AchievementViewModel.java - added bindable properties, changed how comments are exposed to layout

--- app/src/main/java/com/troutslaps/goodwallchallenge/viewmodel/AchievementViewModel.java
+++ app/src/main/java/com/troutslaps/goodwallchallenge/viewmodel/AchievementViewModel.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import com.android.databinding.library.baseAdapters.BR;
 import com.troutslaps.goodwallchallenge.R;
+import com.troutslaps.goodwallchallenge.app.Constants;
 import com.troutslaps.goodwallchallenge.app.Utils;
 import com.troutslaps.goodwallchallenge.model.Achievement;
 import com.troutslaps.goodwallchallenge.model.Comment;
@@ -16,6 +17,8 @@ import com.troutslaps.goodwallchallenge.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.realm.Sort;
+
 /**
  * Created by duchess on 12/02/2017.
  */
@@ -24,7 +27,8 @@ public class AchievementViewModel extends BaseObservable implements PostViewMode
     private final static int NumberOfComments = 2;
     private Context context;
     private Achievement achievement;
-    private List<CommentViewModel> commentViewModels;
+    private CommentViewModel firstComment;
+    private CommentViewModel secondComment;
     private Listener listener;
 
     @Bindable
@@ -34,42 +38,71 @@ public class AchievementViewModel extends BaseObservable implements PostViewMode
         this.context = context;
         this.achievement = achievement;
         this.listener = listener;
+    }
 
+    @Bindable
+    public CommentViewModel getSecondComment() {
+        return secondComment;
+    }
+
+    public void setSecondComment(CommentViewModel secondComment) {
+        this.secondComment = secondComment;
+    }
+
+    @Bindable
+    public CommentViewModel getFirstComment() {
+        return firstComment;
+    }
+
+    public void setFirstComment(CommentViewModel firstComment) {
+        this.firstComment = firstComment;
+    }
+
+    @Bindable
+    public Achievement getAchievement() {
+        return achievement;
     }
 
     public void setAchievement(Achievement achievement) {
         this.achievement = achievement;
-        List<Comment> lastComments = achievement.getLastComments(NumberOfComments);
-        this.commentViewModels = new ArrayList<>();
-        CommentViewModel cvm;
-        for (Comment comment : lastComments) {
-            cvm = new CommentViewModel(comment, context, null);
-            commentViewModels.add(cvm);
+        List<Comment> lastComments = achievement.getComments().sort(Constants.Fields.Created,
+                Sort.DESCENDING);
+
+        if(getSecondComment() == null) {
+            setSecondComment(new CommentViewModel(null, context, listener));
         }
-        notifyPropertyChanged(BR.achievementViewModel);
-    }
-
-
-    public int getCommentVisibility(int position) {
-        return commentViewModels.size() > position ? View.VISIBLE : View.GONE;
-    }
-
-    public CommentViewModel getCommentViewModel(int position) {
-        if (commentViewModels.size() > position) {
-            return commentViewModels.get(position);
+        if(lastComments.size() > 0 && lastComments.get(0) != null) {
+            getSecondComment().setComment(lastComments.get(0));
         } else {
-            return null;
+            getSecondComment().setComment(null);
         }
+
+        if(getFirstComment() == null) {
+            setFirstComment(new CommentViewModel(null, context, listener));
+        }
+        if(lastComments.size() > 1 && lastComments.get(1) != null) {
+            getFirstComment().setComment(lastComments.get(1));
+        } else {
+            getFirstComment().setComment(null);
+        }
+        notifyChange();
     }
 
+
+
+
+
+    @Bindable
     public String getPostTimeAgo() {
         return Utils.DateTime.getTimeAgo(context, achievement.getTimelineDate());
     }
 
+    @Bindable
     public String getAchievementTitle() {
         return achievement.getTitle();
     }
 
+    @Bindable
     public String getAchievementDate() {
         String start = Utils.DateTime.getFormattedDate(achievement.getStartDate());
         String end = Utils.DateTime.getFormattedDate(achievement.getCreated());
@@ -82,6 +115,7 @@ public class AchievementViewModel extends BaseObservable implements PostViewMode
         return String.format(context.getString(R.string.achievement_date_start), end);
     }
 
+    @Bindable
     public String getAchievementBody() {
         Location location = achievement.getLocation();
         String locality = Utils.Locality
@@ -96,20 +130,24 @@ public class AchievementViewModel extends BaseObservable implements PostViewMode
         }
     }
 
+    @Bindable
     public String getAuthorName() {
         User user = achievement.getAuthor();
         return user.getDisplayName();
     }
 
+    @Bindable
     @Override
     public String getCommentBody() {
         return achievement.getBody();
     }
 
+    @Bindable
     @Override
     public String getCommentTime() {
         return Utils.DateTime.getDisplayTime(achievement.getCreated());
     }
+
 
     @Override
     public View.OnClickListener onAuthorNameClicked() {
@@ -126,6 +164,7 @@ public class AchievementViewModel extends BaseObservable implements PostViewMode
         return Utils.Locality.getLocalityDisplay(context, achievement.getAuthor().getLocation());
     }
 
+    @Bindable
     public String getCommentsDisplay() {
         if (achievement.getCommentsCount() == 0) {
             return context.getString(R.string.add_a_comment);
