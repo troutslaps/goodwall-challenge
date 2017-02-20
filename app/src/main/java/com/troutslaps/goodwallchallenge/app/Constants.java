144cae6568d260ce22256029a6452063ac8bd46d
Mai Cervantes <kikiam@gmail.com>
2017-02-21 01:24:25 +0800

    Constants.java - added constants for savedInstanceState values

--- app/src/main/java/com/troutslaps/goodwallchallenge/app/Constants.java
+++ app/src/main/java/com/troutslaps/goodwallchallenge/app/Constants.java
@@ -25,8 +25,9 @@ public class Constants {
         public final static String Modified = "modified";
         public final static String Author = "author";
         public final static String Achievement = "achievement";
-        public final static String  Achievements = "achievements";
+        public final static String Achievements = "achievements";
         public final static String AchievementId = "achievementId";
+        public final static String PendingComment = "pendingComment";
     }
 
 }
