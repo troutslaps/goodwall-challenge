daed1ec54a40769840a675555f8e92719de11cf5
Mai Cervantes <kikiam@gmail.com>
2017-02-22 00:36:04 +0800

    Application.java - added initialisation of BigImage library

--- app/src/main/java/com/troutslaps/goodwallchallenge/app/Application.java
+++ app/src/main/java/com/troutslaps/goodwallchallenge/app/Application.java
@@ -1,5 +1,7 @@
 package com.troutslaps.goodwallchallenge.app;
 
+import com.github.piasy.biv.BigImageViewer;
+import com.github.piasy.biv.loader.glide.GlideImageLoader;
 import com.scopely.fontain.Fontain;
 
 import io.realm.Realm;
@@ -15,5 +17,6 @@ public class Application extends android.app.Application {
         super.onCreate();
         Realm.init(getApplicationContext());
         Fontain.init(this, "Karla");
+        BigImageViewer.initialize(GlideImageLoader.with(this.getApplicationContext()));
     }
 }
